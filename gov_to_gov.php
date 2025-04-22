30b9e77e1361edd5af23c95d413c2bb61704b606
luxsmith656 <luxsmith656@.com>
2025-04-22 11:58:31 +0800

    Made cache modular

--- gov_to_gov.php
+++ gov_to_gov.php
@@ -1,17 +1,5 @@
 <?php
-session_start();
-
-// Prevent caching to stop browser back button access after logout
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-header("Expires: 0");
-
-
-if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
-    exit();
-}
+include 'session.php';
 $pageTitle = "Gov to Gov - MWPD Filing System";
 include '_head.php';
 ?>
